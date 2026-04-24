f74cd14ae0e1d9a0f04e3d9753a59a15a5e26a0c
Rudram Sharma <rudram2505@gmail.com>
2026-04-25 00:33:15 +0530

    Add circular linked list node structure and functions

---
+++ 28.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printCircular(struct Node* head, int n) {
+    if (head == NULL) return;
